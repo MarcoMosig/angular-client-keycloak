8eef620b0c2b3f6062b2fb9771fdbaa3492744c1
“Marco <m.mosig@2m-software.de>
2023-05-28 14:59:09 +0200

    add docker compose file

---
+++ microservice1/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+// Enable CORS support
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.get('/v1/healthcheck', function (req, res) {
+  res.json({message: 'I am healthy Microservice'});
+});
+
+app.use('*', function (req, res) {
+  res.send('Not found!');
+});
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
