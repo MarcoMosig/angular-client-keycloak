bd0f2b73fa5214bebeb5592249e18ccd32ddb4b6
Marco Mosig <marco.mosig@inveos.com>
2023-06-23 17:42:01 +0200

    change stocklist layout

--- client/src/app/features/stocks/stocks.module.ts
+++ client/src/app/features/stocks/stocks.module.ts
@@ -4,27 +4,25 @@ import { StockListComponent } from './components/stock-list/stock-list.component
 import { StocksRoutingModule } from './stocks-rounting.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { StockComponent } from './stock.component';
-import { StockCardComponent } from './components/stock-card/stock-card.component';
 import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {MatRippleModule} from "@angular/material/core";
-import {MatSortModule} from "@angular/material/sort";
-import {MatTableModule} from "@angular/material/table";
-import {MatTabsModule} from "@angular/material/tabs";
-import {NgApexchartsModule} from "ng-apexcharts";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatRippleModule } from "@angular/material/core";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   declarations: [
     StockListComponent,
-    StockCardComponent,
     StockComponent,
   ],
   imports: [
